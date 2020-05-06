c803b40dd7310748d44a8417f483f6889d5601ed
dasshit <56739036+dasshit@users.noreply.github.com>
2020-05-06 14:23:55 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+from io import open
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+setup(name='Python-Mango-Office-API',
+      version='3.6.8',
+      description='API wrapper for Mango Office.',
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      author='dasshit',
+      author_email='v.korobov@mangotele.com',
+      url='https://github.com/dasshit/Python-Mango-Office-API',
+      packages=['mangoapi'],
+      license='GPL2',
+      keywords='mangooffice mango',
+      install_requires=['requests', 'hashlib', 'json', 'random', 'time', 'os', 'datetime'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      ]
+      )
